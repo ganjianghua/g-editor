896454bf4c84595ed3072e55fda24ac4e605235d
ganjianghua <ganjianghua@baidu.com>
2023-03-12 16:37:43 +0800

    feat: 初始化项目

---
+++ src/Editor/bricks/index.ts
@@ -0,0 +1,11 @@
+export const component = [
+  {
+    type: "image",
+    name: "图片",
+    config: {
+      src: "",
+      width: -1,
+      height: -1,
+    },
+  },
+];
---
+++ src/Editor/model/editorModel.ts
@@ -0,0 +1,30 @@
+import { Emitter } from "../../utils/Emitter";
+
+interface Events {
+  add: [];
+}
+
+export class EditorModel extends Emitter<Events> {
+  private modules: any[] = [];
+
+  constructor() {
+    super();
+    this.modules = [];
+  }
+
+  public addBrick() {
+    this.modules.push({
+      type: "image",
+      name: "图片",
+      config: {
+        src: "",
+        width: -1,
+        height: -1,
+      },
+    });
+    this.emit('add');
+  }
+  public getModules() {
+    return this.modules;
+  }
+}
